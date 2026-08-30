989c9e8e93f14815a1a63714d8a8cf2e056e564f
ATERA <73095441+vanoalfa@users.noreply.github.com>
2026-08-30 15:01:35 +0700

    uji coba ke progam PD

--- program/Atera's Final Program/config.py
+++ program/Atera's Final Program/config.py
@@ -1,24 +1,23 @@
-# PID
-PID_KP = 2.0      # Kp Value
-PID_KD = 0.5      # Kd Value
+#PD Tunning
+PID_KP = 2.0      #Kp Value
+PID_KD = 0.5      #Kd Value
 
-# IMU Parameter
-SETPOINT = 0 # Set point IMU's Y axis degree
+#IMU Parameter
+SETPOINT = 0 #Set point IMU's Y axis degree
 
-# Serial ports
-WHEEL_PORT_RIGHT = "COM4"  # Motor controller port
-WHEEL_PORT_LEFT = "COM4"
-IMU_PORT = "COM15"  # Magnetic sensor port
+#Serial ports
+WHEEL_PORT_RIGHT = "/dev/ttyACM0"  #Right Port
+WHEEL_PORT_LEFT = "/dev/ttyACM1" #Left Port
 
-# Baudrates
-WHEEL_BAUDRATE = 115200  # Baudrate for motor controller
-IMU_BAUDRATE = 9600  # Baudrate for sensor
+#Baudrates
+WHEEL_BAUDRATE = 115200  #Baudrate for motor controller
+IMU_BAUDRATE = 9600  #Baudrate for sensor
 
-# Config DDSM 115
+#Config DDSM 115
 CURRENT_LOOP  = 0x01
 VELOCITY_LOOP = 0x02
 POSITION_LOOP = 0x03
 
-TORQUE_CONSTANT_NM_PER_A = 0.75  # Nm per Ampere
-MAX_CURRENT_A = 8.0              # sesuai datasheet: -32767..32767 <=> -8A..8A
+TORQUE_CONSTANT_NM_PER_A = 0.75  #Nm per Ampere
+MAX_CURRENT_A = 8.0              #sesuai datasheet: -32767..32767 <=> -8A..8A
 MIN_CURRENT_A = -8.0
