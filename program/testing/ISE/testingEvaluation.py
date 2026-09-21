491ec2a76c8b41dbd4c9085e7ea8e52b5a6110b1
vanoalfa <73095441+vanoalfa@users.noreply.github.com>
2026-09-21 12:36:01 +0700

    commit 21092026

--- program/testing/ISE/testingEvaluation.py
+++ program/testing/ISE/testingEvaluation.py
@@ -1,23 +1,19 @@
-import numpy as np
+# Source - https://stackoverflow.com/a/50108215
+# Posted by eyllanesc, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-09-14, License - CC BY-SA 3.0
+
+from scipy.signal import *
 from scipy.integrate import quad
+from numpy import *
+p = 10
+na = arange(0,10,1)
+y = []
 
-# Example function: f(x) = sin(x)
-def f(x):
-    return np.sin(x)
+def integrand(t, a, b):
+    return square(t)*cos(b*((2*pi)/a)*t)
 
-# Function to compute mean square over [a, b]
-def mean_square(func, a, b):
-    if a >= b:
-        raise ValueError("Lower limit must be less than upper limit.")
-    
-    # Integrate the square of the function
-    integral, error = quad(lambda x: func(x)**2, a, b)
-    
-    # Mean square value
-    return integral / (b - a)
+for e in na:
+    i,err = quad(integrand,0,p, args=(p, e))
+    y.append(i)
 
-# Example usage
-a, b = 0, np.pi  # Interval
-ms_value = mean_square(f, a, b)
-
-print(f"Mean square of f(x) over [{a}, {b}] is: {ms_value:.6f}")
+print(y)
